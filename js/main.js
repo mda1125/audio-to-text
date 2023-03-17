ef4788b8fa20d63dcc870aff38b37d9187a7a557
Marc David <mda1125@users.noreply.github.com>
2023-03-17 06:34:21 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -44,7 +44,7 @@ async function transcribeAudio() {
     const response = await fetch("https://api.openai.com/v1/audio/translations", {
       method: "POST",
       headers: {
-        "Authorization": "Bearer YOUR_API_KEY"
+        "Authorization": "Bearer ${process.env.WHISPER_API_KEY}"
       },
       body: formData
     });
