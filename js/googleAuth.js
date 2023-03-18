c1298a7a7fa1575fb19c5f2857da2dc8db8f598c
Marc David <mda1125@users.noreply.github.com>
2023-03-18 15:15:20 -0700

    Create googleAuth.js

---
+++ js/googleAuth.js
@@ -0,0 +1,21 @@
+function onSignIn(googleUser) {
+  const id_token = googleUser.getAuthResponse().id_token;
+
+  // Verify the token with your server, and then store it in a secure way
+  sessionStorage.setItem('id_token', id_token);
+
+  // Show the transcription components and the Sign out button
+  document.querySelector('.transcription').style.display = 'block';
+  document.querySelector('button[onclick="signOut()"]').style.display = 'inline';
+}
+
+function signOut() {
+  const auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(() => {
+    sessionStorage.removeItem('id_token');
+
+    // Hide the transcription components and the Sign out button
+    document.querySelector('.transcription').style.display = 'none';
+    document.querySelector('button[onclick="signOut()"]').style.display = 'none';
+  });
+}
