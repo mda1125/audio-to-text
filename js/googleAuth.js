d5269b308abd1ccc765aa7a038bc6d66cb9f9cb6
Marc David <mda1125@users.noreply.github.com>
2023-03-18 15:27:58 -0700

    Update googleAuth.js

--- js/googleAuth.js
+++ js/googleAuth.js
@@ -1,7 +1,11 @@
 function init() {
-  gapi.load('auth2', function() {
+  gapi.load('auth2', function () {
     gapi.auth2.init({
-      client_id: 'YOUR_CLIENT_ID',
+      client_id: '856066838706-e3njg99uk5bj9i1c0g5rff7q8ukf0lm5.apps.googleusercontent.com',
+    }).then(() => {
+      const auth2 = gapi.auth2.getAuthInstance();
+      const signInButton = document.querySelector('.g-signin2');
+      signInButton.addEventListener('click', () => auth2.signIn().then(onSignIn));
     });
   });
 }
